5d23d6ab30edc45d222e2b9613797f9fdf45d8ae
Chris McHardy <chris.mchardy@bbd.co.za>
2025-02-16 16:02:17 +0200

    Update to set path for views

--- sql/R__add_view_for_popular_beans.sql
+++ sql/R__add_view_for_popular_beans.sql
@@ -1,11 +1,13 @@
+SET search_path TO magic_beans_schema;
+
 -- Drop the existing view if it exists
-DROP VIEW IF EXISTS "Top_5_Selling_Beans";
+DROP VIEW IF EXISTS Top_5_Selling_Beans;
 
 -- View for Top 5 Selling Beans
-CREATE VIEW "Top_5_Selling_Beans" AS
+CREATE VIEW Top_5_Selling_Beans AS
 SELECT b.id AS bean_id, b.name AS bean_name, SUM(oi.quantity) AS total_sold
-FROM "order_items" oi
-JOIN "beans" b ON oi.bean_id = b.id
+FROM order_items oi  -- No need to prefix with schema, since search_path is set
+JOIN beans b ON oi.bean_id = b.id
 GROUP BY b.id, b.name
 ORDER BY total_sold DESC
 LIMIT 5;
