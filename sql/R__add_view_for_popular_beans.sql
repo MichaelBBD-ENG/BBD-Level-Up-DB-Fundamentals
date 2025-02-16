bc92fdbc1da8f4c59b98408f883bc8e57762dec4
Chris McHardy <chris.mchardy@bbd.co.za>
2025-02-16 16:17:46 +0200

    Remove comment

--- sql/R__add_view_for_popular_beans.sql
+++ sql/R__add_view_for_popular_beans.sql
@@ -6,7 +6,7 @@ DROP VIEW IF EXISTS Top_5_Selling_Beans;
 -- View for Top 5 Selling Beans
 CREATE VIEW Top_5_Selling_Beans AS
 SELECT b.id AS bean_id, b.name AS bean_name, SUM(oi.quantity) AS total_sold
-FROM order_items oi  -- No need to prefix with schema, since search_path is set
+FROM order_items oi
 JOIN beans b ON oi.bean_id = b.id
 GROUP BY b.id, b.name
 ORDER BY total_sold DESC
