4986093a846ad5364b030f4a265b6a5df4af319a
tebogomotibana <tebogo.motibana@bbd.co.za>
2025-02-18 18:36:11 +0200

    remove data scripts

--- sql/R__OrderTypeData.sql
+++ sql/R__OrderTypeData.sql
@@ -1,12 +1,12 @@
-WITH OrderTypeData AS (
-    SELECT 'Supplier' AS "type"
-    UNION ALL
-    SELECT 'Customer'
-)
-MERGE INTO magic_beans_schema."order_type" AS target
-USING OrderTypeData AS source
-    ON target."type" = source."type"
-WHEN NOT MATCHED BY TARGET THEN
-    INSERT ("type") VALUES (source."type")
-WHEN NOT MATCHED BY SOURCE THEN
-    DELETE;
+-- WITH OrderTypeData AS (
+--     SELECT 'Supplier' AS "type"
+--     UNION ALL
+--     SELECT 'Customer'
+-- )
+-- MERGE INTO magic_beans_schema."order_type" AS target
+-- USING OrderTypeData AS source
+--     ON target."type" = source."type"
+-- WHEN NOT MATCHED BY TARGET THEN
+--     INSERT ("type") VALUES (source."type")
+-- WHEN NOT MATCHED BY SOURCE THEN
+--     DELETE;
