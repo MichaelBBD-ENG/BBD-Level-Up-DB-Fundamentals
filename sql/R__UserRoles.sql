cf596e33d1283f0494c1912d0e847add15bd063f
tebogomotibana <tebogo.motibana@bbd.co.za>
2025-02-18 18:36:11 +0200

    remove data scripts

--- sql/R__UserRoles.sql
+++ sql/R__UserRoles.sql
@@ -1,14 +1,14 @@
-WITH RolesData AS (
-    SELECT 'Admin' AS "role"
-    UNION ALL
-    SELECT 'Driver'
-    UNION ALL
-    SELECT 'Customer'
-)
-MERGE INTO magic_beans_schema."roles" AS target
-USING RolesData AS source
-    ON target."role" = source."role"
-WHEN NOT MATCHED BY TARGET THEN
-    INSERT ("role") VALUES (source."role")
-WHEN NOT MATCHED BY SOURCE THEN
-    DELETE;
+-- WITH RolesData AS (
+--     SELECT 'Admin' AS "role"
+--     UNION ALL
+--     SELECT 'Driver'
+--     UNION ALL
+--     SELECT 'Customer'
+-- )
+-- MERGE INTO magic_beans_schema."roles" AS target
+-- USING RolesData AS source
+--     ON target."role" = source."role"
+-- WHEN NOT MATCHED BY TARGET THEN
+--     INSERT ("role") VALUES (source."role")
+-- WHEN NOT MATCHED BY SOURCE THEN
+--     DELETE;
