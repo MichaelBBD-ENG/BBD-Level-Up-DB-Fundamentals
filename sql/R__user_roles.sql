e82942dfe857099bc4e1f58bf073cee53fdf5074
tebogomotibana <tebogo.motibana@bbd.co.za>
2025-02-20 21:08:40 +0200

    add repeatable insertions

--- sql/R__user_roles.sql
+++ sql/R__user_roles.sql
@@ -1,14 +1,21 @@
--- WITH RolesData AS (
---     SELECT 'Admin' AS "role"
---     UNION ALL
---     SELECT 'Driver'
---     UNION ALL
---     SELECT 'Customer'
--- )
--- MERGE INTO magic_beans_schema."roles" AS target
--- USING RolesData AS source
---     ON target."role" = source."role"
--- WHEN NOT MATCHED BY TARGET THEN
---     INSERT ("role") VALUES (source."role")
--- WHEN NOT MATCHED BY SOURCE THEN
---     DELETE;
+WITH RolesData AS (
+  SELECT 'Admin' AS "role"
+  UNION ALL
+  SELECT 'Driver'
+  UNION ALL
+  SELECT 'Customer'
+),
+ins AS (
+  INSERT INTO magic_beans_schema."roles" ("role")
+  SELECT role FROM RolesData
+  ON CONFLICT ("role") DO NOTHING
+  RETURNING "role"
+),
+del AS (
+  DELETE FROM magic_beans_schema."roles"
+  WHERE "role" NOT IN (SELECT role FROM RolesData)
+  RETURNING "role"
+)
+SELECT 'Inserted' AS operation, role FROM ins
+UNION ALL
+SELECT 'Deleted' AS operation, role FROM del;
