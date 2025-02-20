b81f12378d9898fc7761e0f1ff9bda57f4e176ae
bbdnet3218 <michael.chinyama@bbd.co.za>
2025-02-20 14:48:34 +0200

    updated v0 script

--- sql/V0__init_roles_schemas_users.sql
+++ sql/V0__init_roles_schemas_users.sql
@@ -3,15 +3,25 @@ CREATE SCHEMA IF NOT EXISTS magic_beans_schema;
 
 -- admin
 CREATE ROLE ${admin_role_name} WITH LOGIN PASSWORD '${admin_role_password}';
-GRANT CONNECT ON DATABASE ${database_name} TO ${admin_role_name};
 GRANT USAGE ON SCHEMA magic_beans_schema, public TO ${admin_role_name};
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA magic_beans_schema TO ${admin_role_name};
 
 -- analyst
 CREATE ROLE ${analyst_role_name} WITH LOGIN PASSWORD '${analyst_role_password}';
-GRANT CONNECT ON DATABASE ${database_name} TO ${analyst_role_name};
 GRANT USAGE ON SCHEMA magic_beans_schema, public TO ${analyst_role_name};
 GRANT SELECT ON ALL TABLES IN SCHEMA magic_beans_schema TO ${analyst_role_name};
+
+-- actions
+-- CREATE ROLE ${analyst_role_name} WITH LOGIN PASSWORD '${analyst_role_password}';
+-- GRANT USAGE ON SCHEMA magic_beans_schema, public TO ${analyst_role_name};
+-- GRANT SELECT ON ALL TABLES IN SCHEMA magic_beans_schema TO ${analyst_role_name};
+-- 
+-- -- admin
+-- CREATE USER admin WITH PASSWORD '${admin_role_password}';
+-- -- actions
+-- CREATE USER actions WITH PASSWORD '${admin_role_password}';
+-- -- analyst
+-- CREATE USER analyst WITH PASSWORD 'securepassword';
 
 -- revoke access
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
