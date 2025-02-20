aa5f6c31368767e19d217aa1d42771b9cac83177
Peo Vukea <peovukea-bbd>
2025-02-20 15:57:09 +0200

    Fix primary keys

--- sql/V1__create_tables.sql
+++ sql/V1__create_tables.sql
@@ -1,16 +1,16 @@
 CREATE TABLE magic_beans_schema."supplier" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(255) UNIQUE NOT NULL,
   "contact_id" INT NOT NULL
 );
 
 CREATE TABLE magic_beans_schema."supplier_orders_status" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "status" VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE magic_beans_schema."supplier_orders" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "admin_id" INT,
   "order_date" DATE DEFAULT (CURRENT_TIMESTAMP),
   "supplier_id" INT,
@@ -19,14 +19,14 @@ CREATE TABLE magic_beans_schema."supplier_orders" (
 );
 
 CREATE TABLE magic_beans_schema."supplier_order_items" (
-  "id" int PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "quantity" int,
   "supplier_orders_id" int,
   "bean_id" int
 );
 
 CREATE TABLE magic_beans_schema."order" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "user_id" INT NOT NULL,
   "order_date" DATE DEFAULT (CURRENT_TIMESTAMP),
   "payment_id" INT NOT NULL,
@@ -34,31 +34,31 @@ CREATE TABLE magic_beans_schema."order" (
 );
 
 CREATE TABLE magic_beans_schema."order_history" (
-  "id" int PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "order_id" INT NOT NULL,
   "date" DATE DEFAULT (CURRENT_TIMESTAMP),
   "order_status_id" INT NOT NULL
 );
 
 CREATE TABLE magic_beans_schema."order_item" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "order_id" INT NOT NULL,
   "bean_id" INT NOT NULL,
   "quantity" INT NOT NULL
 );
 
 CREATE TABLE magic_beans_schema."payment_method" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "method" VARCHAR(100)
 );
 
 CREATE TABLE magic_beans_schema."order_status" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "status" VARCHAR(100)
 );
 
 CREATE TABLE magic_beans_schema."bean" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(255) UNIQUE NOT NULL,
   "description" TEXT,
   "price" DECIMAL(10,2) NOT NULL,
@@ -66,19 +66,19 @@ CREATE TABLE magic_beans_schema."bean" (
 );
 
 CREATE TABLE magic_beans_schema."inventory" (
-  "id" int PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "bean_id" int NOT NULL,
   "quantity" int NOT NULL
 );
 
 CREATE TABLE magic_beans_schema."magical_property" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(255) UNIQUE NOT NULL,
   "description" TEXT
 );
 
 CREATE TABLE magic_beans_schema."user" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "first_name" VARCHAR(100) NOT NULL,
   "last_name" VARCHAR(100) NOT NULL,
   "username" VARCHAR(100) UNIQUE NOT NULL,
@@ -86,25 +86,25 @@ CREATE TABLE magic_beans_schema."user" (
 );
 
 CREATE TABLE magic_beans_schema."user_roles" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "user_id" INT NOT NULL,
   "role_id" INT NOT NULL
 );
 
 CREATE TABLE magic_beans_schema."roles" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "role" VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE magic_beans_schema."contact_information" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "phone" VARCHAR(20) UNIQUE,
   "email" VARCHAR(255) UNIQUE,
   "address" TEXT
 );
 
 CREATE TABLE magic_beans_schema."payment" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "order_id" INT NOT NULL,
   "payment_method_id" INT NOT NULL,
   "payment_date" DATE DEFAULT (CURRENT_TIMESTAMP),
@@ -112,7 +112,7 @@ CREATE TABLE magic_beans_schema."payment" (
 );
 
 CREATE TABLE magic_beans_schema."delivery" (
-  "id" INT PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "order_id" INT NOT NULL,
   "driver_id" INT NOT NULL,
   "delivery_notes" TEXT
